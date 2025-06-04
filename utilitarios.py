0ffe226e4dc9fec4f09416f9dfb05cc8b8123667
Fernando Almeida <fernando.oab81@outlook.com>
2025-06-04 20:33:49 -0300

    criando um outro arquivo, só para ter a funções

---
+++ utilitarios.py
@@ -0,0 +1,14 @@
+from categoria import Categoria
+from transacao import Transacao
+
+LISTA_CATEGORIAS = []
+LISTA_TRANSACOES = []
+
+def cadastrar_categoria():
+    pass
+
+def cadastrar_transacao():
+    pass
+
+def saldo_total():
+    pass
